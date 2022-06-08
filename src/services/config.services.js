42d23e4906daa570fd22f33ef841c45564c7e6ea
unknown <cesyrub@gmail.com>
2022-06-08 17:22:53 +0200

    redeploy

--- src/services/config.services.js
+++ src/services/config.services.js
@@ -7,16 +7,12 @@ const service = axios.create({
 
 // es donde hacemos codigo magia donde el token será enviado al backend
 service.interceptors.request.use((config) => {
-
     // buscar el token en localStorage
     const authToken = localStorage.getItem("authToken")
-
     if (authToken) {
         config.headers = { authorization: `Bearer ${authToken}` }
     }
-
     return config
-
 })
 
 export default service
